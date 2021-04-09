exthmui-devices/android_device_lenovo_jd2019

jd2019: add prebuilt dtbo

BoardConfig.mk
@@ -37,6 +37,8 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_PREBUILT_KERNEL := device/lenovo/jd2019/prebuilt/Image.gz-dtb
+BOARD_PREBUILT_DTBOIMAGE := device/lenovo/jd2019/prebuilt/dtbo.img
+BOARD_KERNEL_SEPARATED_DTBO := true
 
 # Audio
 #USE_CUSTOM_AUDIO_POLICY := 1
@@ -64,6 +66,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456

lzgmc/device_lenovo_kunlun2

jd2019/BoardConfig.mk
@@ -33,6 +33,8 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_PREBUILT_KERNEL := device/lenovo/kunlun2/prebuilt/Image.gz-dtb
+BOARD_PREBUILT_DTBOIMAGE := device/lenovo/kunlun2/prebuilt/dtbo.img
+BOARD_KERNEL_SEPARATED_DTBO := true
 
 # Audio
 #USE_CUSTOM_AUDIO_POLICY := 1
@@ -60,6 +62,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456