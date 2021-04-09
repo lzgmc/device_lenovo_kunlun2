744d36e055a2ec83374a52e98e71651d7c83ecd7
lzgmc <lazyguymc@lzgmc.tk>
2021-04-08 19:40:47 -0700

    jd2019: Add jd2019 to makefiles

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_kunlun2.mk
+    $(LOCAL_DIR)/aosp_kunlun2.mk \
+		$(LOCAL_DIR)/aosp_jd2019.mk
+COMMON_LUNCH_CHOICES := \
+		aosp_kunlun2-userdebug \
+		aosp_jd2019-userdebug
